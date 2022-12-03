e8e6e0310e70fc68ae9e09b4dfcff9875f9a8877
rakiduam <spartayotros@gmail.com>
2022-12-03 13:01:36 -0300

    add comparacion.xlsm

--- funciones.py
+++ funciones.py
@@ -1,9 +1,8 @@
-
-import numpy as np
-import re
 # import statsmodels.api as sm
 
 def texotNormalizado(texto):
+    import numpy as np
+    import re
     # texto = re.sub(pattern='^\W*|\W*$', repl='', string=texto)
     # texto = re.sub(pattern='\W{2,}', repl='', string=texto)
     texto = re.sub(pattern='^\s*|\s*$', repl='', string=texto)
@@ -13,7 +12,7 @@ def texotNormalizado(texto):
 
 def midTexto(texto, mid=10):
     mid = int(mid)
-    mid=mid-1
+    mid = mid-1
     return(texto[:mid])
 
 
